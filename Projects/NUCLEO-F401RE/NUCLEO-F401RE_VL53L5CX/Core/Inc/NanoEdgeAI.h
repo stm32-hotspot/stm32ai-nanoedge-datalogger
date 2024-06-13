f6e37be26016b12c00a7c8e106e39c41bb6390b1
Lucas VALVERDE <lucas.valverde@st.com>
2024-06-13 11:38:17 +0200

    Add TOF VL53L5CX for NUCLEO-F401RE

---
+++ Projects/NUCLEO-F401RE/NUCLEO-F401RE_VL53L5CX/Core/Inc/NanoEdgeAI.h
@@ -0,0 +1,94 @@
+/* =============
+Copyright (c) 2023, STMicroelectronics
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that
+the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this list of conditions and the
+  following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the
+  following disclaimer in the documentation and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holders nor the names of its contributors may be used to endorse or promote
+  products derived from this software without specific prior written permission.
+
+*THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER / OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*
+*/
+
+#ifndef __NANOEDGEAI_H__
+#define __NANOEDGEAI_H__
+
+/* Includes */
+#include <stdint.h>
+
+/* Define */
+#define NEAI_ID "661d2d4ce51ddf9dd30d8d96"
+#define AXIS_NUMBER 1
+#define DATA_INPUT_USER 64
+#define MINIMUM_ITERATION_CALLS_FOR_EFFICIENT_LEARNING 20
+
+#ifndef __NEAI_STATE__
+#define __NEAI_STATE__
+enum neai_state { 
+    NEAI_OK = 0,
+    NEAI_INIT_FCT_NOT_CALLED = 123,
+    NEAI_BOARD_ERROR,
+    NEAI_KNOWLEDGE_BUFFER_ERROR,
+    NEAI_NOT_ENOUGH_CALL_TO_LEARNING, //This is a fail-safe to prevent users from learning one or even no signals.
+    NEAI_MINIMAL_RECOMMENDED_LEARNING_DONE,
+    NEAI_UNKNOWN_ERROR};
+#endif
+
+/* Function prototypes */
+#ifdef __cplusplus
+extern "C" {
+#endif
+	/**
+	 * @brief  Initialization can be run at the beginning to initialize the model.
+	 *         Later to initialize a new model and reset all knowledge.
+	 * @retval NEAI_OK in case of success.
+	 */
+	enum neai_state neai_anomalydetection_init(void);
+
+	/**
+	 * @brief  This function is used to learn patterns in your data.
+	 *         It can be used at any time, in the beginning to build the original knowledge base of the AI model.
+	 *         But also later, as an additional learning phase to complement the existing knowledge.
+	 * @param  data_input[] [IN]: Signal to be learn of size AXIS_NUMBER * DATA_INPUT_USER
+	 * @retval NEAI_NOT_ENOUGH_CALL_TO_LEARNING or NEAI_MINIMAL_RECOMMENDED_LEARNING_DONE.
+	 */
+	enum neai_state neai_anomalydetection_learn(float data_input[]);
+
+	/**
+	 * @brief  This function returns a similarity percentage.
+	 *         The mathematical distance between the incoming signal and the existing knowledge.
+	 * @param  data_input  [IN] : size of each buffer
+	 * @param  *similarity [OUT]: The percentage of similarity [0-100] ("100" means completely similar, and "0" completely dissimilar).
+	 * @retval NEAI_OK in case of success
+	 */
+	enum neai_state neai_anomalydetection_detect(float data_input[], uint8_t *similarity);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+/* =============
+Here some sample declaration added in your main program for the use of the NanoEdge AI library.
+You can directly copy these declarations or modify the names.
+* WARNING: respect the size of the buffer.
+
+uint8_t similarity = 0; // Point to similarity (see argument of neai_anomalydetection_detect fct)
+float input_user_buffer[DATA_INPUT_USER * AXIS_NUMBER]; // Buffer of input values
+*/
+
---
+++ Projects/NUCLEO-F401RE/NUCLEO-F401RE_VL53L5CX/Core/Src/main.c
@@ -0,0 +1,456 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "vl53l5cx_api.h"
+#include "cycle_dwt.h"
+#include "NanoEdgeAI.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/************************************************************ NEAI algorithm defines begin ************************************************************/
+/************************************************************ Global settings part ************************************************************/
+#ifndef FRAME_RESOLUTION
+  #define FRAME_RESOLUTION 	64                                    /* 16 if resolution is VL53L5CX_RESOLUTION_4X4 else, 64 if VL53L5CX_RESOLUTION_8X8 */
+#endif
+#ifndef FRAMES
+  #define FRAMES            	1                                     /* Should be between 1 & 32 */
+#endif
+#ifndef DISTANCE_ODR
+  #define DISTANCE_ODR      	15                                    /* Should be between 1 -> 60Hz for VL53L5CX_RESOLUTION_4X4 and 1 -> 15Hz for VL53L5CX_RESOLUTION_8X8 */
+#endif
+/************************************************************ Datalogger / NEAI mode part ************************************************************/
+#ifndef NEAI_MODE
+  #define NEAI_MODE         	0                                     /* 0: Datalogger mode, 1: NEAI functions mode */
+#endif
+#if (NEAI_MODE == 1)
+  #ifndef NEAI_LEARN_NB
+    #define NEAI_LEARN_NB   	20                                    /* Number of buffers to be learn by the NEAI library */
+  #endif
+#endif
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+UART_HandleTypeDef huart2;
+
+/* USER CODE BEGIN PV */
+uint8_t frame_counter = 0, neai_similarity = 0, neai_state = 0;
+volatile uint8_t vl53l5_data_ready = 0;
+uint16_t neai_cnt = 0;
+float neai_time = 0.0;
+float neai_buffer[FRAME_RESOLUTION * FRAMES] = {0.0};
+VL53L5CX_Configuration Dev; /* Sensor configuration */
+VL53L5CX_ResultsData Results; /* Results data from VL53L5CX */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
+/* USER CODE BEGIN PFP */
+static void vl53l5cx_initialize(void);
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+  Dev.platform.address = VL53L5CX_DEFAULT_I2C_ADDRESS;
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_I2C1_Init();
+  /* USER CODE BEGIN 2 */
+  KIN1_InitCycleCounter();
+  KIN1_EnableCycleCounter();
+  vl53l5cx_initialize();
+  if (NEAI_MODE) {
+    neai_state = neai_anomalydetection_init();
+    printf("Initialize NEAI library. NEAI init return: %d.\n",  neai_state);
+  }
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    if(vl53l5_data_ready) {
+      vl53l5cx_get_ranging_data(&Dev, &Results);
+      for(uint16_t i = 0; i < FRAME_RESOLUTION; i++) {
+        neai_buffer[(frame_counter * FRAME_RESOLUTION) + i] =
+        (Results.target_status[VL53L5CX_NB_TARGET_PER_ZONE * i] != 0xFF) ? Results.distance_mm[VL53L5CX_NB_TARGET_PER_ZONE * i] : 0;
+      }
+      if (frame_counter < FRAMES - 1) {
+        frame_counter++;
+      }
+      else {
+        vl53l5cx_stop_ranging(&Dev);
+        frame_counter = 0;
+#if NEAI_MODE
+        uint32_t cycles_cnt = 0;
+        if (neai_cnt < NEAI_LEARN_NB) {
+          neai_cnt++;
+          KIN1_ResetCycleCounter();
+          neai_state = neai_anomalydetection_learn(neai_buffer);
+          cycles_cnt = KIN1_GetCycleCounter();
+          neai_time = (cycles_cnt * 1000000.0) / HAL_RCC_GetSysClockFreq();
+          printf("Learn: %d / %d. NEAI learn return: %d. Cycles counter: %ld = %.1f µs at %ld Hz.\n",
+          neai_cnt, NEAI_LEARN_NB, neai_state, cycles_cnt, neai_time, HAL_RCC_GetSysClockFreq());
+        }
+        else {
+          KIN1_ResetCycleCounter();
+          neai_state = neai_anomalydetection_detect(neai_buffer, &neai_similarity);
+          cycles_cnt = KIN1_GetCycleCounter();
+          neai_time = (cycles_cnt * 1000000.0) / HAL_RCC_GetSysClockFreq();
+          printf("Similarity: %d / 100. NEAI detect return: %d. Cycles counter: %ld = %.1f µs at %ld Hz.\n",
+          neai_similarity, neai_state, cycles_cnt, neai_time, HAL_RCC_GetSysClockFreq());
+        }
+#else
+        for (uint16_t i = 0; i < (uint16_t) (FRAME_RESOLUTION * FRAMES); i++) {
+          printf("%.3f ", neai_buffer[i]);
+        }
+        printf("\n");
+#endif
+        vl53l5cx_start_ranging(&Dev);
+      }
+      vl53l5_data_ready = 0;
+    }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.ClockSpeed = 400000;
+  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 115200;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, PWR_EN_Pin|LPn_Pin, GPIO_PIN_SET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(I2C_RST_GPIO_Port, I2C_RST_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : VL53L5_INT_Pin */
+  GPIO_InitStruct.Pin = VL53L5_INT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(VL53L5_INT_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : LD2_Pin */
+  GPIO_InitStruct.Pin = LD2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PWR_EN_Pin I2C_RST_Pin LPn_Pin */
+  GPIO_InitStruct.Pin = PWR_EN_Pin|I2C_RST_Pin|LPn_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI4_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI4_IRQn);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+/**
+  * @brief  Redirecting stdout to USART1 which is connected on the STLINK port
+  * @retval
+  * @param
+  */
+int __io_putchar(int ch)
+{
+  uint8_t c[1];
+  c[0] = ch & 0x00FF;
+  HAL_UART_Transmit(&huart2, &*c, 1, 10);
+  return ch;
+}
+
+/**
+  * @brief  VL53L5CX initialization function with the defined settings
+  * @retval
+  * @param
+  */
+static void vl53l5cx_initialize()
+{
+  uint8_t status, isAlive;
+
+  /* Switch off / on sensor to reset it */
+  HAL_GPIO_WritePin(GPIOB, PWR_EN_Pin, GPIO_PIN_RESET);
+  HAL_Delay(10);
+  HAL_GPIO_WritePin(GPIOB, PWR_EN_Pin, GPIO_PIN_SET);
+
+  /* Check if there is a VL53L5CX sensor connected */
+  do {
+    HAL_Delay(10);
+    status = vl53l5cx_is_alive(&Dev, &isAlive);
+  } while(!isAlive || status);
+
+  /* Init VL53L5CX sensor */
+  status = vl53l5cx_init(&Dev);
+
+  /* Check if tof is ready */
+  if(status) {
+    printf("VL53L5CX ULD Loading failed\n");
+    while(1);
+  }
+
+  /* Set desired parameters */
+  vl53l5cx_set_ranging_frequency_hz(&Dev, DISTANCE_ODR);
+  vl53l5cx_set_resolution(&Dev, FRAME_RESOLUTION);
+  vl53l5cx_set_ranging_mode(&Dev, VL53L5CX_RANGING_MODE_CONTINUOUS);
+  vl53l5cx_start_ranging(&Dev);
+
+  /* Reset interrupt to avoid bad values after reset / boot */
+  vl53l5_data_ready = 0;
+}
+
+/**
+  * @brief  EXTI line detection callback.
+  * @param  GPIO_Pin: Specifies the port pin connected to corresponding EXTI line.
+  * @retval None
+  */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  switch (GPIO_Pin) {
+  case VL53L5_INT_Pin:
+    vl53l5_data_ready = 1;
+    break;
+  }
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
